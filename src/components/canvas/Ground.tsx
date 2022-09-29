0cb5877586565cdc1aa2487a3f74c45fa4ebf224
Jude Twycross <judetwycross@Judes-Air.Dlink>
2022-09-29 14:36:34 +0200

    start: ground

---
+++ src/components/canvas/Ground.tsx
@@ -0,0 +1,19 @@
+import * as THREE from 'three'
+import { useTexture } from '@react-three/drei'
+import { usePlane } from '@react-three/cannon'
+
+export const Ground = (props) => {
+  const texture = useTexture({
+    map: './assets/ground/diff.jpg',
+    normalMap: './assets/ground/norm.jpeg',
+    roughnessMap: './assets/ground/rough.jpg',
+    displacementMap: './assets/ground/disp.png',
+  })
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeGeometry args={[1000, 1000]} />
+      <meshStandardMaterial {...texture} />
+    </mesh>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
+import { Ground } from '@/components/canvas/Ground'
+import { Physics } from '@react-three/cannon'
+import { PointerLockControls, Sky } from '@react-three/drei'
 import dynamic from 'next/dynamic'
-// Step 5 - delete Instructions components
-import Instructions from '@/components/dom/Instructions'
-// import Shader from '@/components/canvas/Shader/Shader'
 
 // Dynamic import is used to prevent a payload when the website start that will include threejs r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
 // If something goes wrong go back to a static import to show the error.
 // https://github.com/pmndrs/react-three-next/issues/49
-const Shader = dynamic(() => import('@/components/canvas/Shader/Shader'), {
-  ssr: false,
-})
+// const Shader = dynamic(() => import('@/components/canvas/Shader/Shader'), {
+//   ssr: false,
+// })
 
 // dom components goes here
 const Page = (props) => {
   return (
     <>
-      <Instructions />
+      <></>
     </>
   )
 }
@@ -24,7 +24,17 @@ const Page = (props) => {
 // It will receive same props as Page component (from getStaticProps, etc.)
 Page.r3f = (props) => (
   <>
-    <Shader />
+    <Sky sunPosition={[100, 20, 100]} />
+    <ambientLight intensity={0.3} />
+    <Physics>
+      <Ground />
+    </Physics>
+    {/* <PointerLockControls
+      addEventListener={undefined}
+      hasEventListener={undefined}
+      removeEventListener={undefined}
+      dispatchEvent={undefined}
+    /> */}
   </>
 )
 
