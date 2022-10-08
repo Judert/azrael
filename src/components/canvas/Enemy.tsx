9f990aa2c74aa7d9824412edbc810a11ffa0e464
Jude Hendri Twycross <22615938@sun.ac.za>
2022-10-08 19:16:40 +0200

    idk

--- src/components/canvas/Enemy.tsx
+++ src/components/canvas/Enemy.tsx
@@ -4,13 +4,14 @@ import { useCylinder, useSphere, useBox } from '@react-three/cannon'
 import { useThree, useFrame } from '@react-three/fiber'
 import { MapContext } from '@/lib/context'
 
-const SPEED = 5
+const SPEED = 0.02
 
 const direction = new THREE.Vector3()
 const frontVector = new THREE.Vector3()
 const sideVector = new THREE.Vector3()
 const rotation = new THREE.Vector3()
 const speed = new THREE.Vector3()
+const raycaster = new THREE.Raycaster()
 
 export const Enemy = (props) => {
   const [ref, api] = useBox(() => ({
@@ -19,28 +20,33 @@ export const Enemy = (props) => {
     ...props,
   }))
   const [map, setMap] = useContext(MapContext)
+  const position = useRef([0, 0, 0])
+  useEffect(() => api.position.subscribe((p) => (position.current = p)), [])
   const velocity = useRef([0, 0, 0])
   useEffect(() => api.velocity.subscribe((v) => (velocity.current = v)), [])
-  useFrame((state) => {
-    // set rotation to face the player
-    const player = state.scene.getObjectByName('player')
-    api.rotation.set(
-      0,
-      Math.atan2(
-        player.position.x - ref.current.position.x,
-        player.position.z - ref.current.position.z
-      ),
-      0
-    )
-    // if (player) {
-    //   api.rotation.
-    // }
-
-    // setMap(
-    //   map.map((row, r) =>
-    //     row.map((color, c) => (r === i && c === j ? newColor : color))
-    //   )
-    // )
+  const rotation = useRef([0, 0, 0])
+  useEffect(() => api.rotation.subscribe((r) => (rotation.current = r)), [])
+  useFrame((state, delta, frame) => {
+    for (let i = 0; i < 360; i += 3) {
+      const angle = (i * Math.PI) / 180
+      const x = Math.cos(angle)
+      const z = Math.sin(angle)
+      direction.set(x, 0, z)
+      raycaster.set(ref.current.position, direction)
+      const intersects = raycaster.intersectObjects(state.scene.children)
+      if (intersects.length > 0) {
+        if (intersects[0].object.name === 'player') {
+          // api.rotation.set(rotation.current[0], angle, rotation.current[2])
+          // api.velocity.set(x * SPEED, velocity.current[1], z * SPEED)
+          // break
+          api.position.set(
+            direction.x * SPEED,
+            position.current[1],
+            direction.z * SPEED
+          )
+        }
+      }
+    }
   })
   return (
     <>
